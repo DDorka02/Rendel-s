f83d11183d3c8cff15165a54f639288a41f9882f
DDorka02 <dobszaydorka@gmail.com>
2024-04-18 12:12:29 +0200

    rendelés oldal létrehozása, és tervezése

---
+++ adatok.js
@@ -0,0 +1,34 @@
+export const VIRAG = [
+    {
+    kep:"kepek/tulipan.png",
+    nev:"Tulipán",
+    elet:2,
+    szine:"piros"
+},
+{
+    kep:"kepek/napraforgo.jpg",
+    nev:"Napraforgó",
+    elet:2,
+    szine:"sárga"
+},
+{
+    kep:"kepek/Bazsarozsa.jpg",
+    nev:"Bazsarózsa",
+    elet:2,
+    szine:"rózsaszín"
+},
+{
+    kep:"kepek/jacint.jpg",
+    nev:"Jácint",
+    elet:2,
+    szine:"lila"
+},
+{
+    kep:"kepek/GYongyvirag.jpg",
+    nev:"Gyöngyvirág",
+    elet:2,
+    szine:"fehér"
+}
+]
+
+console.log(VIRAG)
---
+++ fuggvenyek.js
@@ -0,0 +1,3 @@
+export function tablazatLetrehozasa(lista){
+    
+}
